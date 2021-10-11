331ff3b5663df32d479fd6cd94c32985296b2803
k-szulc <s-q-n@protonmail.com>
2021-10-11 17:00:30 +0200

    Implemented interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/xyz/itbs/szupetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package xyz.itbs.szupetclinic.services;
+
+import xyz.itbs.szupetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}

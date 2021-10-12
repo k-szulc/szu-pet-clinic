b43ca9fce4db7b992a5310336b935f25586afa99
k-szulc <s-q-n@protonmail.com>
2021-10-12 18:52:45 +0200

    CrudService interface has been added, closes #11

--- pet-clinic-data/src/main/java/xyz/itbs/szupetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/xyz/itbs/szupetclinic/services/OwnerService.java
@@ -2,15 +2,9 @@ package xyz.itbs.szupetclinic.services;
 
 import xyz.itbs.szupetclinic.model.Owner;
 
-import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastNme(String lastName);
 
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/xyz/itbs/szupetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/xyz/itbs/szupetclinic/services/VetService.java
@@ -3,13 +3,7 @@ package xyz.itbs.szupetclinic.services;
 
 import xyz.itbs.szupetclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long>{
 
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }

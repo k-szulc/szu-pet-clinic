531e5db7a12e4cf3e77ba2ce0719475c5e197124
k-szulc <kamil.szulc@hotmail.com>
2021-11-22 12:16:21 +0100

    refactored IndexControllerTest to use SpringExtension instead of MockitoExtension

--- pet-clinic-web/src/test/java/xyz/itbs/szupetclinic/controllers/IndexControllerTest.java
+++ pet-clinic-web/src/test/java/xyz/itbs/szupetclinic/controllers/IndexControllerTest.java
@@ -3,23 +3,21 @@ package xyz.itbs.szupetclinic.controllers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes={IndexController.class})
 class IndexControllerTest {
 
-    @InjectMocks
+    @Autowired
     IndexController indexController;
 
     MockMvc mockMvc;

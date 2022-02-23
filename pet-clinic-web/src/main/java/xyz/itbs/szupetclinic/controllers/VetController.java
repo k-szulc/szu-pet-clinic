5dde1a75eef05d02f048ec0796eef781db46d180
k-szulc <kamil.szulc@hotmail.com>
2022-02-23 16:56:35 +0100

    implemented /api/vets endpoint - closes #60

--- pet-clinic-web/src/main/java/xyz/itbs/szupetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/xyz/itbs/szupetclinic/controllers/VetController.java
@@ -2,11 +2,15 @@ package xyz.itbs.szupetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import xyz.itbs.szupetclinic.model.Vet;
 import xyz.itbs.szupetclinic.services.VetService;
 
+import java.util.Set;
+
 @Controller
-@RequestMapping("/vets")
 public class VetController {
 
     private final VetService vetService;
@@ -15,9 +19,14 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/index", "/index.html"})
+    @GetMapping({"/vets/index", "/vets/index.html"})
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+        return vetService.findAll();
     }
 }
